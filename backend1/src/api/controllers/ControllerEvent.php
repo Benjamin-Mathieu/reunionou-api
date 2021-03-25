12f448d44ef79504d2454763cf8fed058abb9b3d
Punkill <giratina54@hotmail.fr>
2021-03-25 10:45:36 +0100

    Ajout user_id a createEvent a test

--- backend1/src/api/controllers/ControllerEvent.php
+++ backend1/src/api/controllers/ControllerEvent.php
@@ -86,11 +86,11 @@ class ControllerEvent
         $event->title = filter_var($body->title, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $event->description = filter_var($body->description, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $event->date = $body->date;
-        $event->user_id = 1;
+        $event->user_id = $req->getAttribute('token')->user;
         $event->token = bin2hex(random_bytes(32));
         $event->adress = filter_var($body->adress, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $event->public = 1;
-        $event->main_event = 1;
+        $event->public = $body->public;
+        $event->main_event = $body->main_event;
 
         try {
             $event->save();
